6dd99ce4b32cc6b28baba0d568ea5c85c6c12a3e
wayphaxx <39088031+wayphaxx@users.noreply.github.com>
2018-05-09 11:37:50 +0200

    Update Test.cs

--- Tester/Test.cs
+++ Tester/Test.cs
@@ -11,10 +11,11 @@ namespace Tester
 		static void Main(string[] args)
 		{			
 		 
-		string[] s= DnsMxsRecord.DnsMx.GetMXRecords("umom.org"); //change the string to fit whatever server we're looking for
+		string[] s= DnsMx.GetMXRecords("umom.org"); //change the string to fit whatever server we're looking for
 		foreach (string st in s)
 				Console.WriteLine("Server: {0}",st);
 			Console.ReadLine();	
 		}
 	}
 }
+
